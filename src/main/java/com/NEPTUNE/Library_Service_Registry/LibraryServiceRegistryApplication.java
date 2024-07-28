0b56319c4515581cfca82c8cf5703a32d2ff7a0a
abhijeetagnihotri123 <abhijeetagnihotri5@gmail.com>
2024-07-28 22:49:30 +0530

    Service Registry added

--- src/main/java/com/NEPTUNE/Library_Service_Registry/LibraryServiceRegistryApplication.java
+++ src/main/java/com/NEPTUNE/Library_Service_Registry/LibraryServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.NEPTUNE.Library_Service_Registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class LibraryServiceRegistryApplication {
 
 	public static void main(String[] args) {
