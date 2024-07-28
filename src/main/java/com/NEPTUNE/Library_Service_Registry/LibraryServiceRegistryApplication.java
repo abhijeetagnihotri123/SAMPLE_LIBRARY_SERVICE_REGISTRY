e90db2e667cd02c79497a36d23074a677dfc1928
abhijeetagnihotri123 <abhijeetagnihotri5@gmail.com>
2024-07-28 18:24:46 +0530

    Service Registry code added

---
+++ src/main/java/com/NEPTUNE/Library_Service_Registry/LibraryServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.NEPTUNE.Library_Service_Registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NEPTUNE/Library_Service_Registry/LibraryServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NEPTUNE.Library_Service_Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
